65087236980e48921ba6cef96b31f277ef727229
Raul Neto <raulalvarezneto@gmail.com>
2023-08-29 17:52:45 -0300

    alterçao nos videos

--- video.js
+++ video.js
@@ -2,9 +2,18 @@ const videoPlayer = document.getElementById('video-player');
 const videoSource = document.getElementById('video-source');
 
 const videoList = [
-    'vdo/vdo01.mp4',
-    'vdo/vdo02.mp4',
-    'vdo/vdo03.mp4'
+    'vdo/01.mp4',
+    'vdo/02.mp4',
+    'vdo/03.mp4',
+    'vdo/04.mp4',
+    'vdo/05.mp4',
+    'vdo/06.mp4',
+    'vdo/07.mp4',
+    'vdo/08.mp4',
+    'vdo/09.mp4',
+    'vdo/10.mp4',
+    'vdo/11.mp4',
+    'vdo/12.mp4',
 ];
 
 let currentVideoIndex = 0;
